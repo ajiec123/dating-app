a0a6a4c4aae352857cc855c19f220908cc1a17cd
Rahmat Pratama <ajirahmat17@gmail.com>
2023-11-04 22:26:51 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2018 <div class="bullet"></div> Made with &hearts; by <a
+            href="https://artisan1337.com/">Artisan</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="#">Administrator</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="#">AD</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Products Menu</li>
+
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown"><i class="fas fa-chalkboard-teacher"></i>
+                    <span>Manage All Product</span></a>
+            </li>
+            <li class="active">
+                <a class="nav-link" href="blank.html"><i class="far fa-plus-square"></i>
+                    <span>Add New Product</span>
+                </a>
+            </li>
+
+            <li class="menu-header">Orders Menu</li>
+            <li class="dropdown">
+                <a href="#"><i class="fas fa-store"></i>
+                    <span>Manage All Orders</span></a>
+            </li>
+
+            <li class="dropdown">
+                <a href="#"><i class="fas fa-pen"></i>
+                    <span>Add New Orders</span></a>
+            </li>
+
+            <li class="menu-header">Users Menu</li>
+
+            <li><a class="nav-link" href="credits.html"><i class="fas fa-user-cog"></i>
+                    <span>Manage All Users</span></a></li>
+        </ul>
+
+
+    </aside>
+</div>
